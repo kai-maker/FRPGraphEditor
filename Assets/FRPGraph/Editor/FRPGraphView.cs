6e72dfff969459ebbe0ba99ee781e0857339647d
Kouichi Matsumura <kai4.1.1.2@gmail.com>
2020-10-09 12:39:13 +0900

    Remove unused namespaces

--- Assets/FRPGraph/Editor/FRPGraphView.cs
+++ Assets/FRPGraph/Editor/FRPGraphView.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.Serialization;
 using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
