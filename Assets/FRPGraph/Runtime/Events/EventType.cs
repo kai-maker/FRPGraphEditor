69e7281b2d137d10886abd6fe85fb415c0cd8ccf
Kouichi Matsumura <kai4.1.1.2@gmail.com>
2020-10-10 18:05:42 +0900

    Add EventType class.

---
+++ Assets/FRPGraph/Runtime/Events/EventType.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FRPGraph.Runtime
+{
+    public class EventType
+    {
+        private LinkedList<EventTypePrimitive> _list;
+        private string _baseType;
+        public string BaseType => _baseType;
+        public EventTypePrimitive? OuterType => _list.Count > 0 ? (EventTypePrimitive?) _list.First.Value : null;
+
+        public EventType(string baseType, params EventTypePrimitive[] eventTypes)
+        {
+            _baseType = baseType;
+            _list = eventTypes.Length == 0
+                ? new LinkedList<EventTypePrimitive>()
+                : new LinkedList<EventTypePrimitive>(eventTypes);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var lhs = this;
+            var rhs = obj as EventType;
+            if (rhs == null) return false;
+            var isListEqual = lhs._list.SequenceEqual(rhs._list);
+            var isBaseTypeEqual = lhs._baseType == rhs._baseType;
+            return isListEqual && isBaseTypeEqual;
+        }
+
+        public EventType Clone()
+        {
+            return new EventType(_baseType, _list.ToArray());
+        }
+
+        public EventType Wrap(EventTypePrimitive primitive)
+        {
+            var returnVal = Clone();
+            returnVal._list.AddLast(primitive);
+            return returnVal;
+        }
+
+        public EventType Unwrap()
+        {
+            var returnVal = Clone();
+            returnVal._list.RemoveLast();
+            return returnVal;
+        }
+    }
+
+    public enum EventTypePrimitive
+    {
+        Event, Behaviour
+    }
+}
---
+++ Assets/Tests/EventTypeTest.cs
@@ -0,0 +1,81 @@
+﻿using FRPGraph.Runtime;
+using NUnit.Framework;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Tests
+{
+    public class EventTypeTest
+    {
+        private const EventTypePrimitive _Behaviour = EventTypePrimitive.Behaviour;
+        private const EventTypePrimitive _Event = EventTypePrimitive.Event;
+        
+        [Test]
+        public void EqualityTest1()
+        {
+            Assert.AreEqual(
+                new EventType("T"), 
+                new EventType("T"));
+            Assert.AreNotEqual(
+                new EventType("T"), 
+                new EventType("U"));
+        }
+        
+        [Test]
+        public void EqualityTest2()
+        {
+            Assert.AreEqual(
+                new EventType("T", _Behaviour), 
+                new EventType("T", _Behaviour));
+            Assert.AreNotEqual(
+                new EventType("T", _Behaviour), 
+                new EventType("T", _Event));
+        }
+
+        [Test]
+        public void EqualityTest3()
+        {
+            Assert.AreEqual(
+                new EventType("T", _Behaviour, _Behaviour, _Event), 
+                new EventType("T", _Behaviour, _Behaviour, _Event));
+            Assert.AreNotEqual(
+                new EventType("T", _Behaviour, _Behaviour, _Event), 
+                new EventType("T", _Behaviour, _Behaviour));
+            Assert.AreNotEqual(
+                new EventType("T", _Behaviour, _Behaviour), 
+                new EventType("T", _Behaviour, _Behaviour, _Event));
+        }
+
+        [Test]
+        public void CloneTest()
+        {
+            var original = new EventType("T", _Behaviour, _Behaviour, _Event);
+            var clone = original.Clone();
+            Assert.IsFalse(ReferenceEquals(original, clone));
+            Assert.AreEqual(original, clone);
+        }
+        
+        [Test]
+        public void WrapTest()
+        {
+            var lhs = new EventType("T", _Behaviour, _Behaviour, _Event);
+            var rhs = new EventType("T", _Behaviour, _Behaviour);
+            Assert.AreNotEqual(lhs, rhs);
+            var newRhs = rhs.Wrap(_Event);
+            Assert.AreEqual(lhs, newRhs);
+            Assert.AreNotEqual(lhs, rhs);
+            var newLhs = lhs.Unwrap();
+            Assert.AreEqual(newLhs, rhs);
+            Assert.AreNotEqual(lhs, rhs);
+        }
+        
+        [Test]
+        public void OuterTypeValidate()
+        {
+            var lhs = new EventType("T", _Behaviour, _Behaviour, _Event);
+            var rhs = new EventType("T", _Behaviour, _Behaviour);
+            Assert.AreEqual(lhs.OuterType, _Behaviour);
+            Assert.AreEqual(rhs.OuterType, _Behaviour);
+            Assert.AreEqual(new EventType("U").OuterType, null);
+        }
+    }
+}
