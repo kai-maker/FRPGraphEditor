a750d93cc9c9398288468818b8043d3635de9f1a
Kouichi Matsumura <kai4.1.1.2@gmail.com>
2020-10-11 14:49:01 +0900

    Add OperatorInfo class

---
+++ Assets/FRPGraph/Runtime/Operators/OperatorInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NewFrpGraph.Operators
+{
+    public class OperatorInfo
+    {
+        public readonly string OperatorName;
+        public readonly string OperatorReturnName;
+        public readonly List<string> OperatorArgumentNames;
+
+        public OperatorInfo(string operatorName, string operatorReturnName, params string[] operatorArgumentNames)
+        {
+            OperatorName = operatorName;
+            OperatorReturnName = operatorReturnName;
+            OperatorArgumentNames = operatorArgumentNames.ToList();
+        }
+    }
+
+    public static class OperatorInfoObjects
+    {
+        public static OperatorInfo MapE = new OperatorInfo("MapE", "Event", "Event");
+        public static OperatorInfo MergeE = new OperatorInfo("MergeE", "Event");
+        public static OperatorInfo SwitchE = new OperatorInfo("SwitchE", "Event", "Behavior<Event>");
+        public static OperatorInfo FilterE = new OperatorInfo("FilterE", "Event", "Event");
+        public static OperatorInfo SwitchB = new OperatorInfo("SwitchB", "Behavior", "Behavior<Behavior>");
+    }
+}
---
+++ Assets/Tests/OperatorInfoTest.cs
@@ -0,0 +1,19 @@
+﻿using NewFrpGraph.Operators;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class OperatorInfoTest
+    {
+        [Test]
+        public void EqualityTest1()
+        {
+            var infos = typeof(OperatorInfoObjects).GetFields();
+            foreach (var fieldInfo in infos)
+            {
+                var operatorInfo = (OperatorInfo)fieldInfo.GetValue(null);
+                Assert.AreEqual(operatorInfo.OperatorName.GetType(), typeof(string));
+            }
+        }
+    }
+}
