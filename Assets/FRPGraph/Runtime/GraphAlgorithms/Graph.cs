3590bf6f8bd6236a67580c7462985ae1ca7d2e06
Kouichi Matsumura <kai4.1.1.2@gmail.com>
2021-02-05 17:28:34 +0900

    Done adding cycle graph detection

--- Assets/FRPGraph/Runtime/GraphAlgorithms/Graph.cs
+++ Assets/FRPGraph/Runtime/GraphAlgorithms/Graph.cs
@@ -1,24 +1,148 @@
-﻿namespace FRPGraph.Runtime.GraphAlgorithms
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace FRPGraph.Runtime.GraphAlgorithms
 {
+    /**
+     * Original source code from
+     * https://github.com/mission-peace/interview/tree/master/src/com/interview/graph
+     * by Tushar Roy
+     * 
+     * Ported original Java source code to C-Sharp
+     * by Kouich Matsumura
+     */
     public class Graph<T>
     {
+        private List<Edge<T>> allEdges;
+        private Dictionary<long, Vertex<T>> allVertex;
+        private bool isDirected = false;
+
+        public Graph(bool isDirected)
+        {
+            allEdges = new List<Edge<T>>();
+            allVertex = new Dictionary<long, Vertex<T>>();
+            this.isDirected = isDirected;
+        }
+
+        public void addEdge(long id1, long id2)
+        {
+            addEdge(id1, id2, 0);
+        }
+
+        public void addEdge(long id1, long id2, int weight)
+        {
+            Vertex<T> vertex1 = null;
+            if (allVertex.ContainsKey(id1))
+            {
+                vertex1 = allVertex[id1];
+            }
+            else
+            {
+                vertex1 = new Vertex<T>(id1);
+                allVertex.Add(id1, vertex1);
+            }
+            Vertex<T> vertex2 = null;
+            if (allVertex.ContainsKey(id2))
+            {
+                vertex2 = allVertex[id2];
+            }
+            else
+            {
+                vertex2 = new Vertex<T>(id2);
+                allVertex.Add(id2, vertex2);
+            }
+            
+            var edge = new Edge<T>(vertex1, vertex2, isDirected, weight);
+            allEdges.Add(edge);
+            vertex1.addAdjacentVertex(edge, vertex2);
+            if (!isDirected)
+            {
+                vertex2.addAdjacentVertex(edge, vertex1);
+            }
+        }
+
+        public List<Edge<T>> GetAllEdges() => allEdges;
         
+        public Vertex<T> GetVertex(long id) => allVertex[id];
+
+        public IEnumerable<Vertex<T>> GetAllVertex()
+        {
+            return allVertex.Values;
+        }
+        
+        public override string ToString()
+        {
+            StringBuilder buffer = new StringBuilder();
+            foreach(var edge in GetAllEdges())
+            {
+                buffer.Append(edge.GetVertex1() + " " + edge.GetVertex2() + " " + edge.GetWeight());
+                buffer.Append("\n");
+            }
+            return buffer.ToString();
+        }
     }
     
-    class Vertex<T>
+    public class Vertex<T>
     {
         public long id;
         private T data;
+        private List<Edge<T>> edges = new List<Edge<T>>();
+        private List<Vertex<T>> adjacentVertex = new List<Vertex<T>>();
+
+        public Vertex(long id)
+        {
+            this.id = id;
+        }
+
+        public long GetId() => id;
+
+        public void SetData(T data)
+        {
+            this.data = data;
+        }
+
+        public T GetData() => data;
+
+        public void addAdjacentVertex(Edge<T> e, Vertex<T> v)
+        {
+            edges.Add(e);
+            adjacentVertex.Add(v);
+        }
+
+        public override string ToString() => id.ToString();
+
+        public List<Vertex<T>> GetAdjacentVertexes() => adjacentVertex;
+        public List<Edge<T>> GetEdges() => edges;
+
+        public int GetDegree() => edges.Count;
+
+        public override int GetHashCode()
+        {
+            const int prime = 31;
+            int result = 1;
+            result = prime * result + (int) (id ^ (id >> 32));
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (GetType() != obj.GetType()) return false;
+            var other = (Vertex<T>) obj;
+            return id == other.id;
+        }
     }
 
-    class Edge<T>
+    public class Edge<T>
     {
         private bool isDirected = false;
         private Vertex<T> vertex1;
         private Vertex<T> vertex2;
         private int weight;
 
-        Edge(Vertex<T> vertex1, Vertex<T> vertex2, bool isDirected, int weight)
+        public Edge(Vertex<T> vertex1, Vertex<T> vertex2, bool isDirected, int weight)
         {
             this.vertex1 = vertex1;
             this.vertex2 = vertex2;
@@ -26,10 +150,10 @@
             this.isDirected = isDirected;
         }
 
-        Vertex<T> GetVertex1() => vertex1;
-        Vertex<T> GetVertex2() => vertex2;
+        public Vertex<T> GetVertex1() => vertex1;
+        public Vertex<T> GetVertex2() => vertex2;
 
-        private int getWeight() => weight;
+        public int GetWeight() => weight;
 
         public bool IsDirected() => isDirected;
 
--- Assets/FRPGraph/Runtime/GraphAlgorithms/TarjanStronglyConnectedComponent.cs
+++ Assets/FRPGraph/Runtime/GraphAlgorithms/TarjanStronglyConnectedComponent.cs
@@ -1,7 +1,126 @@
-﻿namespace FRPGraph.Runtime.GraphAlgorithms
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FRPGraph.Runtime.GraphAlgorithms
 {
+    /**
+     * Original source code from
+     * https://github.com/mission-peace/interview/tree/master/src/com/interview/graph
+     * by Tushar Roy
+     * 
+     * Ported original Java source code to C-Sharp
+     * by Kouich Matsumura
+     */
+    
+    /**
+     * Date 08/16/2015
+     * @author Tushar Roy
+     *
+     * Find strongly connected components of directed graph.
+     *
+     * Time complexity is O(E + V)
+     * Space complexity  is O(V)
+     *
+     * Reference - https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
+     */
     public class TarjanStronglyConnectedComponent
     {
-        
+        private Dictionary<Vertex<int>, int> visitedTime;
+        private Dictionary<Vertex<int>, int> lowTime;
+        private HashSet<Vertex<int>> onStack;
+        private Stack<Vertex<int>> stack;
+        private HashSet<Vertex<int>> visited;
+        private List<HashSet<Vertex<int>>> result;
+        private int time;
+
+        public List<HashSet<Vertex<int>>> scc(Graph<int> graph)
+        {
+            time = 0;
+            
+            visitedTime = new Dictionary<Vertex<int>, int>();
+            
+            lowTime = new Dictionary<Vertex<int>, int>();
+            
+            onStack = new HashSet<Vertex<int>>();
+            
+            stack = new Stack<Vertex<int>>();
+            
+            visited = new HashSet<Vertex<int>>();
+            
+            result = new List<HashSet<Vertex<int>>>();
+
+            foreach (Vertex<int> vertex in graph.GetAllVertex())
+            {
+                if (visited.Contains(vertex))
+                {
+                    continue;
+                }
+                sccUtil(vertex);
+            }
+            return result;
+        }
+
+        private void sccUtil(Vertex<int> vertex)
+        {
+            visited.Add(vertex);
+            visitedTime.Add(vertex, time);
+            lowTime.Add(vertex, time);
+            time++;
+            stack.Push(vertex);
+            onStack.Add(vertex);
+
+            foreach (var child in vertex.GetAdjacentVertexes())
+            {
+                if (!visited.Contains(child))
+                {
+                    sccUtil(child);
+
+                    lowTime[vertex] = Math.Min(lowTime[vertex], lowTime[child]);
+                }
+                else if (onStack.Contains(child))
+                {
+                    lowTime[vertex] = Math.Min(lowTime[vertex], visitedTime[child]);
+                }
+            }
+
+            if (visitedTime[vertex] == lowTime[vertex])
+            {
+                HashSet<Vertex<int>> stronglyConnectedComponent = new HashSet<Vertex<int>>();
+                Vertex<int> v;
+                do
+                {
+                    v = stack.Pop();
+                    onStack.Remove(v);
+                    stronglyConnectedComponent.Add(v);
+                } while (!vertex.Equals(v));
+                result.Add(stronglyConnectedComponent);
+            }
+        }
+        public static void Test(string[] args) {
+            Graph<int> graph = new Graph<int>(true);
+            graph.addEdge(1,2);
+            graph.addEdge(2,3);
+            graph.addEdge(3,1);
+            graph.addEdge(3,4);
+            graph.addEdge(4,5);
+            graph.addEdge(5,6);
+            graph.addEdge(6,4);
+            graph.addEdge(7,6);
+            graph.addEdge(7,8);
+            graph.addEdge(8,7);
+
+            TarjanStronglyConnectedComponent tarjanStronglyConnectedComponent = new TarjanStronglyConnectedComponent();
+            List<HashSet<Vertex<int>>> result = tarjanStronglyConnectedComponent.scc(graph);
+
+            result.ForEach(scc =>
+            {
+                foreach (var vertex in scc)
+                {
+                    Debug.Log(vertex + " ");
+                }
+            });
+        }
     }
 }
